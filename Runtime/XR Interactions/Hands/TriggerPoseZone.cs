4b4e38ca34d7b33f17b10fce2ddeaedca7a89cb4
tyrant117 <nslange@ucdavis.edu>
2024-01-30 20:57:19 -0800

    trigger poses

--- Runtime/XR Interactions/Hands/TriggerPoseZone.cs
+++ Runtime/XR Interactions/Hands/TriggerPoseZone.cs
@@ -10,15 +10,30 @@ namespace VaporXR
         private HandPoseDatum _pose;
         [SerializeField]
         private float _duration;
+        [SerializeField]
+        private Collider _collider;
+        [SerializeField]
+        private List<Collider> _ignoreColliders;
 
         private VXRHand _currentHand;
 
+        private void Awake()
+        {
+            foreach(var col in _ignoreColliders)
+            {
+                Physics.IgnoreCollision(_collider, col, true);
+            }
+        }
 
         private void OnTriggerEnter(Collider other)
         {
             var hand = other.GetComponentInParent<VXRHand>();
             if (hand)
             {
+                if (_currentHand)
+                {
+                    _currentHand.FallbackToIdle(_duration);
+                }
                 _currentHand = hand;
                 _currentHand.SetHandPose(_pose.Value, duration: _duration);
             }
